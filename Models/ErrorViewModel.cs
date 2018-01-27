6c3de408e94bdc7fb552ef38ee51de428fe06205
Xoadra <xijp77@gmail.com>
2018-01-27 00:14:18 -0800

    Initial repo established for Angular and ASP.NET Core integration exploration.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,19 @@
+
+
+
+
+using System;
+
+
+namespace NgCore.Models {
+	public class ErrorViewModel {
+		
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty( RequestId );
+		
+	}
+}
+
+
+
