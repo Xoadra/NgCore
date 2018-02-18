178f76e6457517ba8c68bf956eb9929c2af37f18
Xoadra <xijp77@gmail.com>
2018-02-18 02:58:34 -0800

    Got css, js, and lib directory contents to compile with Webpack.

--- Views/vendor.ts
+++ Views/vendor.ts
@@ -11,8 +11,26 @@ import '@angular/router'
 
 import 'rxjs'
 
-import 'bootstrap'
-import 'jquery'
+import '../Views/lib/bootstrap/dist/js/bootstrap'
+/* import '../Views/lib/bootstrap/dist/js/npm' */
+import '../Views/lib/bootstrap/dist/fonts/glyphicons-halflings-regular.eot'
+import '../Views/lib/bootstrap/dist/fonts/glyphicons-halflings-regular.svg'
+import '../Views/lib/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf'
+import '../Views/lib/bootstrap/dist/fonts/glyphicons-halflings-regular.woff'
+import '../Views/lib/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2'
+import '../Views/lib/bootstrap/dist/css/bootstrap-theme'
+import '../Views/lib/bootstrap/dist/css/bootstrap'
+
+import '../Views/lib/jquery/dist/jquery'
+import '../Views/lib/jquery-validation/dist/additional-methods'
+import '../Views/lib/jquery-validation/dist/jquery.validate'
+import '../Views/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive'
+
+
+import '../Views/js/site'
+
+import '../Views/css/site'
+
 
 /* import '/lib/bootstrap/dist/css/bootstrap.css'
 import '/lib/bootstrap/dist/js/bootstrap.min.js'
--- webpack.config.js
+++ webpack.config.js
@@ -18,15 +18,12 @@ module.exports = {
 		polyfills: './Views/polyfills.ts',
 		vendor: './Views/vendor.ts'
 	},
-	resolve: {
-		extensions: [ '.ts', '.js' ]
-	},
+	resolve: { extensions: [ '.ts', '.js', '.css' ] },
 	module: {
 		rules: [
 			{
 				test: /\.ts$/,
 				loaders: [
-					/* 'awesome-typescript-loader', */
 					{
 						loader: 'awesome-typescript-loader',
 						options: { configFileName: path.resolve( __dirname, 'tsconfig.json' ) }, 
@@ -35,22 +32,8 @@ module.exports = {
 				]
 			},
 			{ test: /\.(cshtml|html)$/, loader: 'html-loader' },
-			{ test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/, loader: 'file-loader' },
+			{ test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/, loader: 'file-loader?name=images/[name].bundle.[ext]' },
 			{ test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
-			/* {
-				test: /\.css$/,
-				exclude: path.resolve( __dirname, 'app' ),
-				loader: Extract.extract( { fallbackLoader: 'style-loader', loader: 'css-loader?sourceMap' } )
-			}, */
-			{
-				test: /\.css$/,
-				include: [
-					path.resolve( __dirname, 'app' ),
-					path.resolve( __dirname, 'css' ),
-					path.resolve( __dirname, 'lib' ),
-				],
-				loader: 'raw-loader'
-			}
 		]
 	},
 	plugins: [
@@ -58,22 +41,15 @@ module.exports = {
 		new Html( { template: 'Views/Shared/_Layout.cshtml' } ),
 		new Extract( '[name].css' ),
 		new Uglify( { sourceMap: true } ),
-		new Webpack.ContextReplacementPlugin(
-			/angular(\\|\/)core(\\|\/)@angular/,
-			/* path.resolve( './Views' ), */
-			{  } // Map of the routes
-		),
-		new Webpack.optimize.CommonsChunkPlugin( {
-			name: [ 'main', 'vendor', 'polyfills' ]
-		} )
+		new Webpack.optimize.CommonsChunkPlugin( { name: [ 'main', 'vendor', 'polyfills' ] } )
 	],
 	output: {
-		/* path: helpers.root( 'Root' ), */
-		path: path.resolve( __dirname, 'Root' ),
-		publicPath: '/',
 		filename: '[name].bundle.js',
-		chunkFilename: '[id].chunk.js'
+		chunkFilename: '[id].chunk.js',
+		path: path.resolve( __dirname, 'Root' ),
+		publicPath: '/'
 	},
 }
+
 
 
